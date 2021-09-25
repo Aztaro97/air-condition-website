d822bd58fd086f2b18e26b51390dca11d7684f9f
aztaro97 <aztaro97@gmail.com>
2021-09-25 16:35:45 +0400

    update

--- js/lighSlider.js
+++ js/lighSlider.js
@@ -1,58 +1,130 @@
-$(document).ready(function() {
-    $('#imageGalleryAnglais').lightSlider({
-        // gallery:true,
-        item:4,
-        auto:true,
-        loop:true,
-        slideMove:1,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:400,
-        responsive : [
-            {
-                breakpoint:800,
-                settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
-                  }
-            },
-            {
-                breakpoint:480,
-                settings: {
-                    item:2,
-                    slideMove:1
-                  }
-            }
-        ]
-    });  
+$(document).ready(function () {
+  $("#imageGalleryAnglais").lightSlider({
+    // gallery:true,
+    item: 4,
+    auto: true,
+    loop: true,
+    slideMove: 1,
+    easing: "cubic-bezier(0.25, 0, 0.25, 1)",
+    speed: 400,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          item: 3,
+          slideMove: 1,
+          slideMargin: 6,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 2,
+          slideMove: 1,
+        },
+      },
+    ],
   });
+});
 
-  $(document).ready(function() {
-    $('#imageGalleryArab').lightSlider({
-        // gallery:true,
-        rtl:true,
-        item:4,
-        auto:true,
-        loop:true,
-        slideMove:1,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:400,
-        responsive : [
-            {
-                breakpoint:800,
-                settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
-                  }
-            },
-            {
-                breakpoint:480,
-                settings: {
-                    item:2,
-                    slideMove:1
-                  }
-            }
-        ]
-    });  
+$(document).ready(function () {
+  $("#imageGalleryArab").lightSlider({
+    // gallery:true,
+    rtl: true,
+    item: 4,
+    auto: true,
+    loop: true,
+    slideMove: 1,
+    easing: "cubic-bezier(0.25, 0, 0.25, 1)",
+    speed: 400,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          item: 3,
+          slideMove: 1,
+          slideMargin: 6,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 2,
+          slideMove: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $("#serviceSlider").lightSlider({
+    item: 4,
+    auto: true,
+    loop: true,
+    slideMove: 1,
+    easing: "cubic-bezier(0.25, 0, 0.25, 1)",
+    speed: 400,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          item: 3,
+          slideMove: 1,
+          slideMargin: 6,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          item: 2,
+          slideMove: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 1,
+          slideMove: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+    $("#serviceSliderArab").lightSlider({
+      item: 4,
+      auto: true,
+      loop: true,
+    rtl: true,
+
+      slideMove: 1,
+      easing: "cubic-bezier(0.25, 0, 0.25, 1)",
+      speed: 400,
+      responsive: [
+        {
+          breakpoint: 800,
+          settings: {
+            item: 3,
+            slideMove: 1,
+            slideMargin: 6,
+          },
+        },
+        {
+          breakpoint: 650,
+          settings: {
+            item: 2,
+            slideMove: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            item: 1,
+            slideMove: 1,
+          },
+        },
+      ],
+    });
   });
